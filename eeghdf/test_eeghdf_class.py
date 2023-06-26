338711382abb55a11fa0da38f21865c5320001b9
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-06-26 20:54:08 +0900

    add: label attribute

--- eeghdf/test_eeghdf_class.py
+++ eeghdf/test_eeghdf_class.py
@@ -6,13 +6,14 @@ fpath = "test.h5"
 def test_add_eeglab():
     reset_file()
     fs = 250
-    ehf = EEGHDFUpdater(fpath,fs=fs)
+    ehf = EEGHDFUpdater(fpath,fs=fs,lables=["x"])
     ehf.add_eeglab("./resources/sample.set")
 
     with h5py.File(fpath) as h5:
         assert h5["origin"].attrs["fs"] == fs
         assert h5["origin"].attrs["count"] == 80
         assert h5["origin/79"].shape == (62,1000)
+        assert h5["origin/79"].attrs["label"] == "x"
 
 def reset_file():
     if(os.path.isfile(fpath)):
