2c35c081765c71b503c40e5bbb80d5b05b33569a
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2023-06-27 10:17:06 +0900

    Add: preprocessing option to EEGHDFController.add_eeglab

--- eeghdf/eeghdf.py
+++ eeghdf/eeghdf.py
@@ -6,7 +6,7 @@ class EEGHDFUpdater(HDFController):
         super().__init__(hdf_path)
         self.fs = fs
         self.labels = lables
-    def add_eeglab(self,input_path:str):
+    def add_eeglab(self,input_path:str,preprocess_func = None):
         for label in self.labels:
             epochs = get_epochs(input_path,label)
             def update_hdf(h5:h5py.File):
@@ -15,5 +15,12 @@ class EEGHDFUpdater(HDFController):
                 for i in range(epochs.shape[0]):
                     dataset = self.increment_dataset(origin_group,epochs[i,:,:])
                     dataset.attrs["label"] = label
+                if preprocess_func is not None:
+                    custom_group = h5.require_group("custom")
+                    custom_group.attrs["fs"] = self.fs
+                    for i in range(epochs.shape[0]):
+                        x = preprocess_func(epochs[i,:,:])
+                        dataset = self.increment_dataset(custom_group,x)
+                        dataset.attrs["label"] = label
             self.update_hdf(update_hdf)
         
--- eeghdf/test_eeghdf_class.py
+++ eeghdf/test_eeghdf_class.py
@@ -1,5 +1,6 @@
 import os
 import h5py
+import numpy as np
 from . import EEGHDFUpdater
 fpath = "test.h5"
 
@@ -15,6 +16,19 @@ def test_add_eeglab():
         assert h5["origin/79"].shape == (62,1000)
         assert h5["origin/79"].attrs["label"] == "x"
 
+def test_add_eeglab_prepro_option():
+    reset_file()
+    fs = 250
+    ehf = EEGHDFUpdater(fpath,fs=fs,lables=["x"])
+    def prepro_func(x:np):
+         return np.ones((2,x.shape[1]))
+    ehf.add_eeglab("./resources/sample.set",preprocess_func=prepro_func)
+
+    with h5py.File(fpath) as h5:
+        assert h5["custom"].attrs["fs"] == fs
+        assert h5["custom"].attrs["count"] == 80
+        assert np.all(h5["custom/79"][()] == np.ones((2,1000)))
+        assert h5["custom/79"].attrs["label"] == "x"
 def reset_file():
     if(os.path.isfile(fpath)):
             os.remove(fpath)
